05bacfdc53c4994ec9d9ffe4d35d3febdf014df2
RoeRL <bimo.17168@gmail.com>
2026-01-03 19:06:40 +0700

    [RR]: Finish Add Task Function

--- TaskServices.cs
+++ TaskServices.cs
@@ -11,32 +11,30 @@ namespace TaskList.Services
         private static string _mHomePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         private static string _mFolderPath = Path.Combine(_mHomePath, ".config", "tasklist");
         private static string _mFilePath = Path.Combine(_mFolderPath, "task.json");
-        private string _defaultJsonContent = "{}";
+        private string _defaultJsonContent = "[{}]";
         
         public void AddTask()
         {
-            
             if (!Directory.Exists(_mFolderPath)) Directory.CreateDirectory(_mFolderPath);
             if (!File.Exists(_mFilePath)) File.WriteAllText(_mFilePath, _defaultJsonContent);
             Guid uniqueId = Guid.NewGuid();
             string newUid = uniqueId.ToString();
             Console.WriteLine("Enter the Task Title: ");
-            string mTitle = Console.ReadLine();
+            string mTitle = Console.ReadLine() ?? "No Title";
             string oldJsonString = File.ReadAllText(_mFilePath);
-            string formattedJsonString = JValue.Parse(oldJsonString).ToString(Formatting.Indented);
-            TaskModel taskModel = new TaskModel
+            List<TaskModel> oldJson = JsonConvert.DeserializeObject<List<TaskModel>>(oldJsonString) ?? new List<TaskModel>();
+            oldJson.Add(new TaskModel
             {
                 Id = newUid,
                 Title = mTitle,
                 Description = "NONE",
                 IsComplete = true,
                 CreatedAt = DateTime.Now
-
-            };
-            string json = JsonConvert.SerializeObject(taskModel, Formatting.Indented);
-            string fullJson = json + "\n" + formattedJsonString;   
-            Console.WriteLine(fullJson);
-            File.WriteAllText(_mFilePath, fullJson);
+            });
+            
+            string updatedJsonString = JsonConvert.SerializeObject(oldJson,  Formatting.Indented);
+            Console.WriteLine(updatedJsonString);
+            File.WriteAllText(_mFilePath, updatedJsonString);
         }
     }
 }
